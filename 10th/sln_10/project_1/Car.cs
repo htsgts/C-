17b75bdbbf5710943a5931db7a65a3dcd0693d61
htsgts <davidmoon824@naver.com>
2024-05-14 09:28:48 +0900

    10주차 실습

---
+++ 10th/sln_10/project_1/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Car
+    {
+        public void Hi()
+        {
+            Console.WriteLine("안녕하세요");
+        }
+        public void Go()
+        {
+            Console.WriteLine("전진하다");
+        }
+    }
+}
---
+++ 10th/sln_10/project_1/MyMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class MyMath
+    {
+        public static double PI = 3.141592;
+    }
+}
