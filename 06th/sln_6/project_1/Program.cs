b74b0395f7945864a6ce8d69c8331dcb6ca6d8d2
htsgts <davidmoon824@naver.com>
2024-05-14 09:04:40 +0900

    6주차 실습

---
+++ 06th/sln_6/project_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열을 초기화하는 방법
+            //첫번째 방법
+            int[] scores1 = new int[5];
+            scores1[0] = 86;
+            scores1[1] = 74;
+            scores1[2] = 81;
+            scores1[3] = 95;
+            scores1[4] = 67;
+
+            //두번째 방법 (가장 기본적인 방식)
+            int[] scores2 = new int[5] {86, 74, 81, 95, 67};
+
+            //세번째 방법
+            int[] scores3 = new int[] { 86, 74, 81, 95, 67 };
+
+            //네번째 방법
+            int[] scores4 = { 86, 74, 81, 95, 67 };
+
+            foreach (var score in scores1)
+            {
+                Console.WriteLine(score);
+            }
+
+            int sum = 0;
+            foreach (var score in scores2)
+            {
+                sum += score;
+            }
+            Console.WriteLine(sum);
+            int average = sum / scores2.Length;
+            Console.WriteLine(average);
+            Console.WriteLine($"Average Score : {average}");
+
+        }
+    }
+}
---
+++ 06th/sln_6/project_2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열 : 같은 데이터 타입을 가진 데이터 집합(고정)
+            int[] alphas = new int[3] {10, 20, 30};
+            foreach (var item in alphas)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //리스트 : 같은 데이터 타입을 가진 데이터들의 집합(가변)
+            List<int> lists = new List<int>() { 15, 25, 35};
+            lists.Add(45);
+            lists.Add(55);
+            foreach (var item in lists)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            lists.Remove(50);
+            lists.RemoveAt(4);
+            lists.Insert(0, 5);
+            foreach (var item in lists)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            List<int> list = new List<int>();
+            for (int i = 10; i < 16; i++)
+            {
+                list.Add(i);
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            
+            list.RemoveAt(2);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            list.Insert(2,2);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //ArrayList : 배열과 달리 컬렉션을 생성할 때 용량을 미리 지정할 필요없이 필요에 따라 자동으로 그 용량이 늘어나거나 줄어든다.
+
+            ArrayList lists2 = new ArrayList();
+            lists2.Add(10);
+            lists2.Add(20);
+            lists2.Add(30);
+            lists2.Add(40);
+            lists2.Add(50);
+
+            foreach (var item in lists2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            lists2.Remove(30);
+            lists2.RemoveAt(2);
+            lists2.Insert(2, 25);
+            foreach (var item in lists2)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
