0802e877d8d1af955a0cb00d42312f748ee6df6f
htsgts <davidmoon824@naver.com>
2024-05-28 09:09:30 +0900

    11주차 실습

--- 11th/sln_11/project_2/Program.cs
+++ 11th/sln_11/project_2/Program.cs
@@ -13,22 +13,6 @@ namespace project_2
             //속성(Property) : 해당 객체의 특징
             //속성은 기본적으로 변수처럼 값을 담는 역할을 하지만 그 값의 객체와 연간이 있다.
             //속성은 객체의 데이터를 저장한다.
-            BoxA boxa = new BoxA(10, 10);
-            Console.WriteLine(boxa.Area());
-            Console.WriteLine();
-
-
-            BoxB boxb = new BoxB(-10, 10);
-            Console.WriteLine(boxb.Area());
-            //boxb.width = 10; //캡슐화로 접근 불가
-            Console.WriteLine();
-
-            BoxC boxC = new BoxC(-10, 10);
-            Console.WriteLine(boxC.Area());
-            boxC.SetWidth(20);
-            boxC.SetHeight(20);
-            Console.WriteLine(boxC.Area());
-            Console.WriteLine();
 
             BoxD boxD = new BoxD(-10, 10);
             Console.WriteLine(boxD.Area());
@@ -38,7 +22,7 @@ namespace project_2
             Console.WriteLine();
 
             BoxE boxE = new BoxE(-10, 10);
-            Console.WriteLine(boxE.Area());
+            Console.WriteLine(20);
             boxE.Width = 10;
             boxE.Height = 20;
             Console.WriteLine(boxE.Area());
